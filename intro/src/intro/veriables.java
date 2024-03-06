214b1444f8ecabdcfa00f353066ab55a5d0854c9
EmirCevre <emircevre1999@hotmail.com>
2024-03-06 16:41:47 +0300

    '.'

---
+++ intro/src/intro/veriables.java
@@ -0,0 +1,57 @@
+package intro;
+
+public class veriables {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		
+		//String position = ("Makine Mühendisi");
+		
+		//System.out.println("Makine Mühendisi");
+		
+		
+		String position = "Makine Teknikeri";
+		System.out.println(position);
+		System.out.println(position);
+		System.out.println(position);
+		System.out.println(position);
+		System.out.println(position);
+		
+		
+		
+		boolean isExist = false;
+		
+		byte number =127;
+		
+		
+		
+		
+		int unitPrice = 195;
+		System.out.println(unitPrice + "TL");
+		
+		
+		double unitPrice2 = 199.99;
+		System.out.println(unitPrice2 + "TL");
+		
+		
+		int number1=25;
+		int number2=50;
+		int total = number1+number2;
+		System.out.println(total);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
